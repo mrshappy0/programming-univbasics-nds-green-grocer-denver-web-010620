d55e7e6ad8d0699347d3f0de05ad6501edbe6bed
Adam  Shappy <ajs11300@gmail.com>
2019-12-25 19:40:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,8 +53,8 @@ def apply_coupons(cart, coupons)
         cart_item_with_coupon = {
           :item => couponed_item_name,
           :price => coupons[counter][:cost] / coupons[counter][:num],
-          :clearance => cart_item[:clearance],
-          :count => coupons[count][:num],
+          :clearance => cart_item[:clearance]
+          #:count => coupons[count][:num],
         }
         cart << cart_item_with_coupon
         cart_item[:count] -= coupons[counter][:num]
