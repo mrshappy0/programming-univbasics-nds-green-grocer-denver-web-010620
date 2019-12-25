6e6379c7370bfe1c0313f7faca0fdb5167c58e64
Adam  Shappy <ajs11300@gmail.com>
2019-12-25 18:46:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,7 +44,7 @@ def apply_coupons(cart, coupons)
   
   while counter < cart.length do 
     cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
-    
+    coupon_item
     counter += 1  
     
   # REMEMBER: This method **should** update cart
