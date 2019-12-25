0902ab6171bc0f957a1a432b2af899983b097b0c
Adam  Shappy <ajs11300@gmail.com>
2019-12-25 19:45:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -72,7 +72,7 @@ def apply_clearance(cart)
   counter = 0 
   while counter < cart.length do 
     if cart[counter][:clearance]
-      cart[counter][:price] = (cart[counter][:price]*0.80).round
+      cart[counter][:price] = (cart[counter][:price]*0.80).round(2)
     end   
     counter += 1 
     
