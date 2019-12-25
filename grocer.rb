1c405a247985ea9d21a242d5d8558b9c31619bca
Adam  Shappy <ajs11300@gmail.com>
2019-12-25 19:20:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -57,7 +57,7 @@ def apply_coupons(cart, coupons)
           :clearance => cart_item[:clearance]
           :count => 
         }
-        cart << 
+        cart << cart_item_with_coupon
     counter += 1  
     
   # REMEMBER: This method **should** update cart
