9353944042b74779d5b390c1e5d506d4847efb81
Adam  Shappy <ajs11300@gmail.com>
2019-12-25 19:56:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -91,7 +91,7 @@ def checkout(cart, coupons)
   # some irritated customers
   
   consolidated_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(consolidated_cart)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   
   total = 0 
