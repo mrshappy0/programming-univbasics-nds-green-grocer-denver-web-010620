c60913778898e82c1aaae101b21b109746c09964
Adam  Shappy <ajs11300@gmail.com>
2019-12-25 19:24:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,7 +44,7 @@ def apply_coupons(cart, coupons)
   
   while counter < cart.length do 
     cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
-    coupon_item_name = "#{coupon[counter][:item]} W/ COUPON"
+    coupon_item_name = "#{coupons[counter][:item]} W/ COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
     if cart_item && cart_item[:count] >= coupons[counter][:num]
       if cart_item_with_coupon
@@ -53,7 +53,7 @@ def apply_coupons(cart, coupons)
       else 
         cart_item_with_coupon = {
           :item => coupon_item_name,
-          :price => coupon[counter][:cost] / coupons[counter][:num],
+          :price => coupons[counter][:cost] / coupons[counter][:num],
           :clearance => cart_item[:clearance]
         }
         cart << cart_item_with_coupon
