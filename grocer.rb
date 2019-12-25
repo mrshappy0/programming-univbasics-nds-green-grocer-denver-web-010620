346d6df66ad74d1b222d676be41273bc02aac08c
Adam  Shappy <ajs11300@gmail.com>
2019-12-25 19:16:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,7 +50,8 @@ def apply_coupons(cart, coupons)
       if cart_item_with_coupon
         cart_item_with_coupon[:count] += coupons[counter][:num]
         cart_item[:count] -= coupons[counter][:num]
-      else cart_item_with_coupon = {}
+      else 
+        cart_item_with_coupon = {}
     counter += 1  
     
   # REMEMBER: This method **should** update cart
